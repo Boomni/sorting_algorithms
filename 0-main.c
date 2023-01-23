4a61971899d77de0756b201decf870145ba1ed29
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-01-23 20:11:58 +0100

    attempted task 0

--- 0-main.c
+++ 0-main.c
@@ -9,13 +9,14 @@
  */
 int main(void)
 {
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
+	int array[] = {19, 48, 95029012, 5282828, 6085633, 7147041, 7251339, 7916142, 8383928};
+	size_t n = sizeof(array) / sizeof(array[0]);
 
-    print_array(array, n);
-    printf("\n");
-    bubble_sort(array, n);
-    printf("\n");
-    print_array(array, n);
-    return (0);
+	print_array(array, n);
+	printf("\n");
+	bubble_sort(array, n);
+	printf("\n");
+	print_array(array, n);
+
+	return (0);
 }
