232074e3d3963d9294950ed700fe78b42f355184
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-01-23 20:17:09 +0100

    attempted task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,6 +15,11 @@ void bubble_sort(int *array, size_t size)
 	bool swapped = true;
 	size_t i;
 
+	if (size < 2)
+	{
+		print_array(array, size);
+		return;
+	}
 	while (swapped)
 	{
 		swapped = false;
