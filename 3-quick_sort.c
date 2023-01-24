8bdd6222fce34a2a8e88992302e9be976591cb9b
Jonathan Boomni <110749205+Boomni@users.noreply.github.com>
2023-01-24 22:47:27 +0100

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -11,31 +11,34 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	if (size < 2) return;
+	if (size < 2)
+		return;
 
 	int pivot = array[size - 1];
-	int i = 0;
-	int j;
+	int i = 0, j;
 
 	for (j = 0; j < size - 1; j++)
 	{
 		if (array[j] <= pivot)
 		{
-			int temp = array[i];
-			array[i] = array[j];
-			array[j] = temp;
-			i++;
-			for(int k = 0; k < size; k++)
+			if (i != j)
+			{
+				int temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
 				print_array(array, size);
+			}
+			i++;
 		}
 	}
 
-	int temp = array[i];
-	array[i] = array[size - 1];
-	array[size - 1] = temp;
-	for(int k = 0; k < size; k++)
+	if (i != j)
+	{
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
 		print_array(array, size);
-	print_array(array, size);
+	}
 
 	quick_sort(array, i);
 	quick_sort(array + i + 1, size - i - 1);
