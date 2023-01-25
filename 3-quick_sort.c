d9a15f52e98aa1d114dc40ba35a0a48ce16236a6
Boomni Jonathan <rejoiceoye1@gmail.com>
2023-01-25 13:17:06 +0100

    attempted task 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -13,6 +13,7 @@ void quick_sort(int *array, size_t size)
 {
 	int pivot = array[size - 1];
 	size_t i = -1, j;
+	int temp;
 
 	if (size < 2 || array == NULL)
 		return;
@@ -24,20 +25,20 @@ void quick_sort(int *array, size_t size)
 			i++;
 			if (i != j)
 			{
-				array[i] ^= array[j];
-				array[j] ^= array[i];
-				array[i] ^= array[j];
+				temp = array[i];
+				array[i] = array[j];
+				array[j] = temp;
 				print_array(array, size);
 			}
 		}
 	}
 
 	i++;
-	if (i != size - 1 && i != j )
+	if (i != size - 1 && i != j)
 	{
-		array[i] ^= array[size - 1];
-		array[size - 1] ^= array[i];
-		array[i] ^= array[size - 1];
+		temp = array[i];
+		array[i] = array[size - 1];
+		array[size - 1] = temp;
 		print_array(array, size);
 	}
 	quick_sort(array, i);
