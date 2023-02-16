d6f310836343168136be3b0d4ca8242e6b201adb
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-02-16 22:27:33 +0100

    Edited my bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,23 +11,20 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int temp;
-	bool swapped = true;
-	size_t i;
+	size_t i, j, temp;
 
 	if (array == NULL || size == 0)
 		return;
-	while (swapped)
+
+	for (i = 0; i < size - 1; i++)
 	{
-		swapped = false;
-		for (i = 0; i < size - 1; i++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[j + 1])
 			{
-				temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
-				swapped = true;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
 			}
 		}
--- 0-main.c
+++ 0-main.c
@@ -9,7 +9,7 @@
  */
 int main(void)
 {
-	int array[] = {19, 48, 95029012, 5282828, 6085633, 7147041, 7251339, 7916142, 8383928};
+	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
 	size_t n = sizeof(array) / sizeof(array[0]);
 
 	print_array(array, n);
