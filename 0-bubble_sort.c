0886181341ac7d73ea99d89edba0f66a29af87dd
Jonathan Boomni <rejoiceoye1@gmail.com>
2023-02-17 09:02:22 +0100

    Edited my bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,13 +11,14 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, temp;
+	size_t i, j, temp, swapped;
 
 	if (array == NULL || size == 0)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
+		swapped = 0;
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -26,7 +27,10 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 				print_array(array, size);
+				swapped = 1;
 			}
 		}
+		if (swapped == 0)
+			break;
 	}
 }
